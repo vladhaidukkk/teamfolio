c22f3d84b9eab344e9ab2bcf39436e54064b51d6
vladhaidukkk <the.vladhaiduk@gmail.com>
2022-03-13 17:35:07 +0200

    add carousel for projects

---
+++ src/components/ui/projectSlide/projectSlide.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+
+const ProjectSlide = ({ img, name }) => {
+  return (
+    <Box
+      boxShadow={3}
+      sx={{
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-end',
+        width: '100%',
+        height: '300px',
+      }}
+    >
+      <img
+        src={img}
+        alt={name}
+        style={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          objectFit: 'cover',
+          objectPosition: 'center',
+        }}
+      />
+      <Typography
+        variant="h1"
+        sx={{
+          zIndex: 2,
+          bgcolor: 'rgba(255,255,255,0.5)',
+          m: 2,
+          py: 1,
+          px: 1.5,
+          fontSize: '28px',
+          fontWeight: 300,
+        }}
+      >
+        {name}
+      </Typography>
+    </Box>
+  );
+};
+
+ProjectSlide.propTypes = {
+  img: PropTypes.string,
+  name: PropTypes.string,
+};
+
+export default ProjectSlide;
