ac98e4ba942108b435535900858a0b0008fcccbf
Roman Adamchik <roman.adamchik@gmail.com>
2022-03-13 14:46:30 +0300

    add props fpr introUserCard

--- src/components/layout/membersListContainer/membersListContainer.jsx
+++ src/components/layout/membersListContainer/membersListContainer.jsx
@@ -19,10 +19,10 @@ const MembersListContainer = () => {
 
   return (
     <>
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}>
-        {usersCrop.map((_, index) => (
+      <Grid container spacing={{ xs: 5, md: 3, lg: 3 }} columns={{ xs: 4, sm: 8, md: 12, lg: 12 }}>
+        {usersCrop.map((user, index) => (
           <Grid item xs={12} sm={4} md={3} lg={3} key={index} style={{ margin: '0 auto' }}>
-            <IntroUserCard />
+            <IntroUserCard user={user} />
           </Grid>
         ))}
       </Grid>
